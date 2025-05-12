0c67918b14c63f162db04ee84762df53cb1ca209
Kimbap <mj4god21@naver.com>
2025-05-12 17:49:58 +0900

    feat: chapter so, enemy so

--- Assets/01_Scripts/System/GameManager.cs
+++ Assets/01_Scripts/System/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoSingleton<GameManager>
     public GameState currentState = GameState.Playing;
     public SkillUIManager skillUIManager;
     public float Exp = 0;
-    public float maxExp;
+    public float maxExp = 30;
     public int nowLevel;
 
     private void Update()
@@ -24,6 +24,8 @@ public class GameManager : MonoSingleton<GameManager>
             nowLevel++;
             LevelUp();
         }
+
+        MainUIManager.Instance.GetEXP_UIUpdate(Exp, maxExp);
     }
 
     private void MakerOnly()
@@ -35,6 +37,8 @@ public class GameManager : MonoSingleton<GameManager>
     {
         currentState = GameState.LevelUp;
         maxExp = 30 + (nowLevel - 1) * 50;
+        Exp = 0;
+        Debug.Log("maxEXP: " + maxExp);
         Time.timeScale = 0f; // 게임 일시정지
         skillUIManager.ShowSkillChoices();
     }
